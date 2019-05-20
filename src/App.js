8cd2ba2d81f270b3df8f01c88dc2c731478eca2e
Jessica Walker <peskywabbit@hotmail.com>
2019-05-20 16:27:53 -0700

    Removed ActivRoom.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+import MessageList from './components/MessageList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCElMHJJZ7XKkJqXnYtgRt4aOGdCZRgHyQ",
+    authDomain: "bloc-chat-react-1222.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-1222.firebaseio.com",
+    projectId: "bloc-chat-react-1222",
+    storageBucket: "bloc-chat-react-1222.appspot.com",
+    messagingSenderId: "530788154608",
+    appId: "1:530788154608:web:9cd6542aad303b0a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: [],
+      activeRoom: " "
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <nav>
+          <header>
+            <h1 className="App-title">Bloc Chat</h1>
+          </header>
+          </nav>
+        <main>
+        <RoomList firebase={firebase} />
+        <MessageList firebase={firebase} />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
