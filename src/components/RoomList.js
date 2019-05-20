9910177b2a9e671ed48a664e91fa829bb7df93ff
Jessica Walker <peskywabbit@hotmail.com>
2019-05-20 11:46:45 -0700

    Create Chat Rooms checkpoint complete

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -6,10 +6,15 @@ class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-    rooms: []
-      }
-      this.roomsRef = this.props.firebase.database().ref('rooms');
+      rooms: [],
+      newRoomName: ""
     }
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+
+    this.createRoom = this.createRoom.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCreateRoomSubmit = this.handleCreateRoomSubmit.bind(this);
+  }
 //set up real-time event listeners for the database
 componentDidMount() {
   this.roomsRef.on('child_added', snapshot => {
@@ -19,21 +24,42 @@ componentDidMount() {
     });
   }
 
+createRoom(newRoomName) {
+  let newRoom = {
+    name: newRoomName
+  };
+  // Save to database
+  this.roomsRef.push(newRoom)
+}
+
+handleChange(event) {
+  this.setState({newRoomName: event.target.value});
+}
+
+handleCreateRoomSubmit(event) {
+  this.createRoom(this.state.newRoomName)
+}
+
 render () {
   return (
-    <section>
-      <ul>
-        {
-          this.state.rooms.map(room =>
-            (
-              <li key={room.key}>
-                {room.name}
-              </li>
+    <div>
+      <section>
+        <ul>
+          {
+            this.state.rooms.map(room => (
+                <li key={room.key}>
+                  {room.name}
+                </li>
+                )
               )
-            )
-          }
-          </ul>
-    </section>
+            }
+            </ul>
+      </section>
+      <form id="create-room">
+        <input type="text" placeholder="Create a room..." name="room-name" value={this.state.newRoomName} onChange={this.handleChange} />
+        <button type="button" onClick={this.handleCreateRoomSubmit}>Submit</button>
+      </form>
+    </div>
     )
   }
 }
